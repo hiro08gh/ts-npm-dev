0a3647b0069d539519a260c25b878b0578b9ba51
hiro08gh <hiro08gh@gmail.com>
2021-04-29 15:41:20 +0900

    chore: change link scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off'
+    "@typescript-eslint/explicit-function-return-type": "off",
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: "detect",
+    },
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log("test1");
+export const test = "test";
